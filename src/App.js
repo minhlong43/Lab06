889569f66e83b79ae1f5f8a3f768cf193cf55f96
DoMinhLong <dominhlong43@gmail.com>
2022-05-09 22:56:40 +0700

    Controlled Forms

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import React from "react";
+import Main from "./components/MainComponent";
+import { BrowserRouter } from "react-router-dom";
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
